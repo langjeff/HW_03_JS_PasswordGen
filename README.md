207d9a666fcb200638522fd3ad03ad9076f879dc
IronJon <lang.jeffrey@gmail.com>
2020-10-11 17:21:19 -0600

    declared variables and functions for prompting password length and character types

--- README.md
+++ README.md
@@ -1,33 +1,31 @@
 # HW_03_JS_PasswordGen
-//Building a password generator with JavaScript//
+## Building a password generator with JavaScript
 
-# Homework Instructions
+# APP CRITERIA & APPROACH
 
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
+### GIVEN I need a new, secure password
+### WHEN I click the button to generate a password
+### THEN I am presented with a series of prompts for password criteria
+### WHEN prompted for password criteria
+### THEN I select which criteria to include in the password
+### WHEN prompted for the length of the password
+### THEN I choose a length of at least 8 characters and no more than 128 characters
+### WHEN prompted for character types to include in the password
+### THEN I choose lowercase, uppercase, numeric, and/or special characters
+### WHEN I answer each prompt
+### THEN my input should be validated and at least one character type should be selected
+### WHEN all prompts are answered
+### THEN a password is generated that matches the selected criteria
+### WHEN the password is generated
+### THEN the password is either displayed in an alert or written to the page
 
 _Action: user clicks button_
-(have event.listener for user click)
 
 - prompt for length between 8 & 128 characters
 - store answer as numeric variable (to be used later in for loop for generating password)
 - create arrays for lowercase, numeric, & special character types (use toUpper() on lowercase for upper case array.)
 
 _confirm: you will need to chose at least one character type for your password_
-if: yes -
 
 - confirm for lowercase
 - store answer as boolean variable
@@ -41,21 +39,10 @@ if: yes -
 _at end of confirms, validate at least 1 true_
 _if all false, return message you must select at least one special character type_
 
-## using if, elseif evaluate returned booleans to concatenate selected arrays.
+## AFTER ABOVE, WRITE FORMULA FOR DETERMINING PASSWORD USING:
+### for loop with Math.floor(Math.random) for entered length. Use push() to store result of each loop to new array to be used in for loop.
 
-## using for loop with Math.floor(Math.random) for entered length. use push() to store result of each loop to new array.
+### Convert password array to string using .join();
 
-## convert password array to string using .join();
+### Place password into html element.
 
-## place password into html element with getElementById & .textContent
-
-#APPROACH
-* came up with global variables for passwordLength, and arrays for values to be used in the password.
-* for generatePassword function there are several components:
-1. prompt for length of password between 8 and 128 characters 
-2. alert if password is too long, or too short.
-3. store booleans for inclusion of lowercase, uppercase, numeric, and special characters.
-4. 
-
-
-" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
--- script.js
+++ script.js
@@ -1,50 +1,77 @@
-// Assignment Code
+// ***************DECLARING GLOBAL VARIABLES FOR PASSWORD GENERATOR*********************
+
+// variable in card footer for 'Generate Password' button
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// create variables for boolean storage of character type confirmations
+var confirmLow;
+var confirmUp;
+var confirmNum;
+var confirmSpec;
+// stores array for alpha characters to be included in password
+var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//stores numberic values for password
+var numChar = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//stores special character values for password
+var specChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+//converts lowercase to uppercase letters
+var toUpper = function (x) {
+  return x.toUpperCase();
+}
+var upCase = lowCase.map(toUpper);
+
+// variable for concatenated array after confirmation of character types to pass to for loop
+var passPool;
+// ************************FUNCTIONS AND ACTIONS*********************************
+
+// Add event listener to generateBtn to execute 'writePassword' function on click
+generateBtn.addEventListener("click", writePassword);
+     
+// declares function to 'writePassword'
 function writePassword() {
+  // calls generate password function and stores result as variable
   var password = generatePassword();
+  // creates variable for html text element with id password
   var passwordText = document.querySelector("#password");
-
+  // passes value of password from 'generatePassword' function to the password element in html
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// DECLARED ARRAYS AND VARIABLES for generatePassword function
-      // ==========================================================================
-
-      var lowerAlpha = [];
-      // stores array for alpha characters to be included in password
-      var numeric = [];
-      //stores numberic values for password
-      var specialChar = [];
-      //stores special character values for password
-      var upperAlpha;
-      // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-      //
-      function passLength() {
-        var pLength = prompt(
-          "Enter password length:",
-          "between 8-128 characters."
-        );
-        if (pLength < 8) {
-          alert("Password too short.");
-        } else if (pLength > 128) {
-          alert("Password too long");
-        }
-        console.log(pLength);
-      }
-
-      function generatePassword() {
-        var lowCase = confirm("Do you want lowercase letters?");
-        console.log(lowCase);
-        var upCase = confirm("Do you want uppercase letters?");
-        console.log(upCase);
-        var numChar = confirm("Do you want numbers?");
-        console.log(numChar);
-        var specChar = confirm("Do you want special characters?");
-        console.log(specChar);
-      }
+// creates function for password generation
+function generatePassword() {
+  // records password length from user input  
+  var pLength = parseInt(prompt(
+    "Enter password length:",
+    "between 8-128 characters."
+  ));
+  // evaluates pLength for length criteria and returns error if not satisfied
+  if (pLength < 8) {
+    alert("Password too short.");
+  } 
+  else if (pLength > 128) {
+    alert("Password too long");
+  }
+  // if password length is acceptable, confirm characters to use and store booleans
+  else {
+  var confirmLow = confirm("Do you want lowercase letters?");
+  console.log(confirmLow);
+  var confirmUp = confirm("Do you want uppercase letters?");
+  console.log(confirmUp);
+  var confirmNum = confirm("Do you want numbers?");
+  console.log(confirmNum);
+  var confirmSpec = confirm("Do you want special characters?");
+  console.log(confirmSpec);
+  };
+  // Evaluates confirm returns to determine types of characters to include in password
+  // if all false
+  //***************************initial code for confirms & arrays */
+  // if(!confirmLow && !confirmUp && !confirmNum && !confirmSpec) {
+  //   passPool= alert.apply("Please choose at least one character type.");
+  // }
+  // if(confirmLow && confirmUp && confirmNum && confirmSpec) {
+  //   passPool= lowCase.concat(upCase,numChar,specChar);
+  // }
+}
